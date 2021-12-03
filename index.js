c5aa7606c033208d34048f4fa5cc4e898da76e9f
ceniatnior <cointrainer1@gmail.com>
2021-12-03 15:56:10 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(array, emotion) {
+    let newArray = []
+    for(let i=0; i < array.length; i++) {
+        newArray.push(`Thank you, ${array[i]}, for the wonderful ${emotion} gift!`)
+    }
+    return newArray;
+}
+
+const num = 10;
+function countDown(num) {
+while(num >= 0 ) {
+    console.log(num)
+    num--
+   }
+   return num;
+}
